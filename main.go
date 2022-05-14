2d2b04543eb8934f57224f79f2471bb863347887
Sungwoong Kang <k900327s@gmail.com>
2022-05-14 22:46:36 +0900

    Rendering Template

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+
+	"github.com/kmnkit/nomadcoin/blockchain"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello")
+	tmpl := template.Must(template.ParseFiles("templates/home.html")) // 이 안에서 에러가 있을 경우 자동으로 처리해 줌
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(w, data)
 }
 
 func main() {
