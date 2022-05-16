bf75a3740312ff76ee7ad43abe1952e76b4ad082
Marco Kang <kmnkit@gmail.com>
2022-05-17 00:08:23 +0900

    Refactoring

--- main.go
+++ main.go
@@ -1,48 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"text/template"
-
-	"github.com/kmnkit/nomadcoin/blockchain"
+	"github.com/kmnkit/nomadcoin/explorer"
 )
-
-const (
-	port string = ":4000"
-	templateDir string = "templates/"
-)
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-func home(w http.ResponseWriter, r *http.Request) {	
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(w, "home", data)	
-}
-
-func add(w http.ResponseWriter, r *http.Request){
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(w, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
-	}
-
-}
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
+	
 }
