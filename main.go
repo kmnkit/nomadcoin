7c6d9efe8533af0cf20221ab1ce85099b4bc6227
Marco Kang <kmnkit@gmail.com>
2022-05-16 23:45:12 +0900

    Template Partials Use

--- main.go
+++ main.go
@@ -9,20 +9,25 @@ import (
 	"github.com/kmnkit/nomadcoin/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port string = ":4000"
+	templateDir string = "templates/"
+)
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
 	Blocks    []*blockchain.Block
 }
 
-func home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.gohtml")) // 이 안에서 에러가 있을 경우 자동으로 처리해 줌
+func home(w http.ResponseWriter, r *http.Request) {	
 	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(w, data)
+	templates.ExecuteTemplate(w, "home", data)	
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
