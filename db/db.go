d1e49c20c9f6cf1ffc8fef3a375c30d7d83c7cb1
Sungwoong Kang <k900327s@gmail.com>
2022-05-18 11:48:58 +0900

    Creating Database

---
+++ db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/kmnkit/nomadcoin/utils"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		// init db
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		db = dbPointer
+		utils.HandleErr(err)
+		err = db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = tx.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
+	}
+	return db
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/kmnkit/nomadcoin
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e // indirect
+)
