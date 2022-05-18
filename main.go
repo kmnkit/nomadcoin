24eae432d5e03ffd01c10313552017714145e43e
Sungwoong Kang <k900327s@gmail.com>
2022-05-18 10:49:56 +0900

    FlagSet

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
@@ -14,15 +15,26 @@ func usage() {
 }
 
 func main() {
+	fmt.Println(os.Args)
 	if len(os.Args) < 2 {
 		usage()
 	}
+
+	// Flag가 많다면 Flagset이 좋지만 하나일 경우는 그닥 ...
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
-		fmt.Println("Start REST API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
+	}
+	if rest.Parsed() {
+		fmt.Println(*portFlag)
+		fmt.Println("Start server🚀")
 	}
 }
