1d5b171aa8f004e05bf950842e996a471fc1a418
Sungwoong Kang <k900327s@gmail.com>
2022-05-18 09:30:26 +0900

    Gorilla Mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kmnkit/nomadcoin
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/kmnkit/nomadcoin/explorer"
 	"github.com/kmnkit/nomadcoin/rest"
 )
 
 func main() {
-	go explorer.Start(3000)
 	rest.Start(4000)
 }
