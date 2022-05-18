7e3dd3b281551f409aefd3cefc91803077e2236f
Sungwoong Kang <k900327s@gmail.com>
2022-05-18 11:17:19 +0900

    CLI Flag

---
+++ cli/cli.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/kmnkit/nomadcoin/explorer"
+	"github.com/kmnkit/nomadcoin/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to 노마드 코인\n")
+	fmt.Printf("Please use the following commands:\n")
+	fmt.Printf("-port=4000:		Set the PORT of the Server\n")
+	fmt.Printf("-mode=rest:		Choose between 'html' and 'rest'\n")
+	os.Exit(0)
+}
+
+func Start() {
+	if len(os.Args) == 1 {
+		usage()
+	}
+	port := flag.Int("port", 4000, "Set port the server\n")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'\n")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		// start rest api
+		rest.Start(*port)
+	case "html":
+		// start html explorer
+		explorer.Start(*port)
+	case "both":
+		go rest.Start(*port)
+		explorer.Start(*port + 1)
+	default:
+		usage()
+	}
+}
--- main.go
+++ main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to 노마드 코인\n")
-	fmt.Printf("Please use the following commands:\n")
-	fmt.Printf("explorer: 	Start the HTML Explorer\n")
-	fmt.Printf("rest: 		Start the REST API(recommended)\n")
-	os.Exit(0)
-}
+import "github.com/kmnkit/nomadcoin/cli"
 
 func main() {
-	fmt.Println(os.Args)
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	// Flag가 많다면 Flagset이 좋지만 하나일 경우는 그닥 ...
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-	if rest.Parsed() {
-		fmt.Println(*portFlag)
-		fmt.Println("Start server🚀")
-	}
+	cli.Start()
 }
