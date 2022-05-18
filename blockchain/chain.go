1432ed35a39ad3cb743d49c44854c65879ffd779
Sungwoong Kang <k900327s@gmail.com>
2022-05-18 20:05:43 +0900

    Difficulty Part.1

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -8,9 +8,15 @@ import (
 	"github.com/kmnkit/nomadcoin/utils"
 )
 
+const (
+	defaultDifficulty  int = 2
+	difficultyInterval int = 5
+)
+
 type blockchain struct {
-	NewestHash string `json:"newestHash"`
-	Height     int    `json:"height"`
+	NewestHash        string `json:"newestHash"`
+	Height            int    `json:"height"`
+	CurrentDifficulty int    `json:"currentDifficulty"`
 }
 
 var b *blockchain
@@ -46,10 +52,20 @@ func (b *blockchain) Blocks() []*Block {
 	return blocks
 }
 
+func (b *blockchain) difficulty() int {
+	if b.Height == 0 {
+		return defaultDifficulty
+	} else if b.Height%difficultyInterval == 0 {
+		// recalculate the difficulty
+	} else {
+		return b.CurrentDifficulty
+	}
+}
+
 func Blockchain() *blockchain {
 	if b == nil {
 		once.Do(func() {
-			b = &blockchain{"", 0}
+			b = &blockchain{Height: 0}
 
 			// search for checkpoint on the db
 			checkpoint := db.Checkpoint()
