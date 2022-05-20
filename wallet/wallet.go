054f7bcb78663d4a30c9c0e9f421f14ebfba3d65
Sungwoong Kang <k900327s@gmail.com>
2022-05-20 16:20:17 +0900

    sign 후 검증까지의 일련의 과정

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -10,14 +10,29 @@ import (
 	"github.com/kmnkit/nomadcoin/utils"
 )
 
+const (
+	hashedMessage string = "1c5863cd55b5a4413fd59f054af57ba3c75c0698b3851d70f99b8de2d5c7338f"
+)
+
 func Start() {
+	// 비공개키 생성
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	utils.HandleErr(err)
+
+	// 메세지를 해쉬 함
 	message := "i love you"
 	hashedMessage := utils.Hash(message)
+	fmt.Println(hashedMessage)
+
+	// 메세지 바이트화
 	hashAsBytes, err := hex.DecodeString(hashedMessage)
 	utils.HandleErr(err)
+
+	// 메세지에 서명함
 	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
 	utils.HandleErr(err)
-	fmt.Printf("R:%d\nS:%d", r, s)
+
+	// 서명을 공개키로 검증
+	ok := ecdsa.Verify(&privateKey.PublicKey, hashAsBytes, r, s)
+	fmt.Println(ok)
 }
