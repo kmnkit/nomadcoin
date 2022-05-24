51418ef0e4b0bd1eb0a6b4befb3d6d205ab34ffc
Sungwoong Kang <k900327s@gmail.com>
2022-05-24 11:17:26 +0900

    persist wallet

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ package main
 import "github.com/kmnkit/nomadcoin/wallet"
 
 func main() {
-	// defer db.Close()
-	// cli.Start()
-	wallet.Start()
+	wallet.Wallet()
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,29 +2,58 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"os"
+
+	"github.com/kmnkit/nomadcoin/utils"
 )
 
 type wallet struct {
 	privateKey *ecdsa.PrivateKey
 }
 
+const (
+	fileName string = "nomadcoin.wallet"
+)
+
 var w *wallet
 
 func hasWalletFile() bool {
-	_, err := os.Stat("nomadcoin.wallet")
+	// has a wallet already?
+	_, err := os.Stat(fileName)
 	return !os.IsNotExist(err)
+}
+
+func createPrivateKey() *ecdsa.PrivateKey {
+	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	utils.HandleErr(err)
+	return privKey
+}
+
+func persistKey(key *ecdsa.PrivateKey) {
+	// key를 받아서 복붙 가능한 형태로 변환함.
+	// 그 byte들을 파일로 저장.
+	// 이 key를 전에 한 것처럼 16진수 문자열로 만들 필요는 없음
+	bytes, err := x509.MarshalECPrivateKey(key)
+	utils.HandleErr(err)
+	err = os.WriteFile(fileName, bytes, 0644)
+	utils.HandleErr(err)
+
 }
 
 func Wallet() *wallet {
 	if w == nil {
+		w = &wallet{}
 		if hasWalletFile() {
 			// yes -> restore from file
-
+		} else {
+			key := createPrivateKey()
+			persistKey(key)
+			w.privateKey = key
 		}
-		// has a wallet already?
 		// no -> create private key, save to file
-
 	}
 	return w
 }
