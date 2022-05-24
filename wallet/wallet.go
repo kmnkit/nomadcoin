f74540f2c9472894adf6ce11268b502ee9af9b2f
Sungwoong Kang <k900327s@gmail.com>
2022-05-24 13:24:59 +0900

    Addresses

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -12,6 +12,7 @@ import (
 
 type wallet struct {
 	privateKey *ecdsa.PrivateKey
+	address    string // 16진수
 }
 
 const (
@@ -40,7 +41,18 @@ func persistKey(key *ecdsa.PrivateKey) {
 	utils.HandleErr(err)
 	err = os.WriteFile(fileName, bytes, 0644)
 	utils.HandleErr(err)
+}
 
+func restoreKey() (key *ecdsa.PrivateKey) { // named return을 하면 미리 초기화가 돼서 return을 안 해도 됨
+	keyAsBytes, err := os.ReadFile(fileName)
+	utils.HandleErr(err)
+	key, err = x509.ParseECPrivateKey(keyAsBytes)
+	utils.HandleErr(err)
+	return
+}
+
+func aFromK(key *ecdsa.PrivateKey) string {
+	//
 }
 
 func Wallet() *wallet {
@@ -48,12 +60,14 @@ func Wallet() *wallet {
 		w = &wallet{}
 		if hasWalletFile() {
 			// yes -> restore from file
+			w.privateKey = restoreKey()
 		} else {
 			key := createPrivateKey()
 			persistKey(key)
 			w.privateKey = key
 		}
 		// no -> create private key, save to file
+		w.address = aFromK(w.privateKey)
 	}
 	return w
 }
